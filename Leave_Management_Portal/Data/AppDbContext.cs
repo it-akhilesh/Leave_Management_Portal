1fdc93b848a010487b7a38d229a3bad5d1f774e2
Akhilesh Singh <360akhileshsingh.it@gmail.com>
2025-08-06 00:53:18 +0530

    Add project files.

---
+++ Leave_Management_Portal/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Leave_Management_Portal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leave_Management_Portal.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Users> User { get; set; }
+    }
+}
---
+++ Leave_Management_Portal/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leave_Management_Portal.ViewModels
+{
+    public class VerifyEmailViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
